1ff52b9a316e8cb032fbcc203822ff450dbdfe3a
Aiden Rathsam <rathsamaiden@gmail.com>
2025-09-16 10:54:59 -0400

    created three difficulties and their global vars

--- ThreeOut_Max_Game.js
+++ ThreeOut_Max_Game.js
@@ -1,6 +1,14 @@
 const prompt = require('prompt-sync')();
+//GLOBAL VARS
+let arithOp;
+const ADD = 0;
+const SUBTRACT = 1;
+const MULTIPLY = 2;
+const DIVIDE = 3;
+const MODULAR = 4;
 
-main();
+//main();
+hardMode();
 
 function main(){
     console.log("Hello! Welcome to Max Out - Three Out the math game");
@@ -17,4 +25,39 @@ function maxScore(){
 
 function threeOut(){
     console.log("threeOut");
+}
+
+function easyMode(){
+    arithOp = Math.floor(Math.random()*2);
+    if(arithOp === ADD)
+        return console.log(`${Math.floor(Math.random()*10)} + ${Math.floor(Math.random()*10)}`);
+    else
+        return console.log(`${Math.floor(Math.random()*10)} - ${Math.floor(Math.random()*10)}`);
+
+}
+function mediumMode(){
+    arithOp = Math.floor(Math.random()*5);
+    if(arithOp === ADD)
+        return console.log(`${Math.floor(Math.random()*100)} + ${Math.floor(Math.random()*100)}`);
+    if(arithOp === SUBTRACT)
+        return console.log(`${Math.floor(Math.random()*100)} - ${Math.floor(Math.random()*100)}`);
+    if(arithOp === MULTIPLY)
+        return console.log(`${Math.floor(Math.random()*10)} * ${Math.floor(Math.random()*10)}`);
+    if(arithOp === DIVIDE)
+        return console.log(`${Math.floor(Math.random()*10)} * ${Math.floor(Math.random()*10)+1}`);
+    if(arithOp === MODULAR)
+        return console.log(`${Math.floor(Math.random()*10)} % ${Math.floor(Math.random()*10)}`);
+}
+function hardMode(){
+    arithOp = Math.floor(Math.random()*5);
+    if(arithOp === ADD)
+        return console.log(`${Math.floor(Math.random()*1000)} + ${Math.floor(Math.random()*1000)}`);
+    if(arithOp === SUBTRACT)
+        return console.log(`${Math.floor(Math.random()*1000)} - ${Math.floor(Math.random()*1000)}`);
+    if(arithOp === MULTIPLY)
+        return console.log(`${Math.floor(Math.random()*100)} * ${Math.floor(Math.random()*10)}`);
+    if(arithOp === DIVIDE)
+        return console.log(`${Math.floor(Math.random()*100)} * ${Math.floor(Math.random()*10)+1}`);
+    if(arithOp === MODULAR)
+        return console.log(`${Math.floor(Math.random()*100)} % ${Math.floor(Math.random()*10)}`);
 }
