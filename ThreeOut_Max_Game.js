485215d6eca69f581fa2db3635c2df983214948c
Aiden Rathsam <rathsamaiden@gmail.com>
2025-09-16 10:11:58 -0400

    Welcome msg and mode selection created - temp entry

--- ThreeOut_Max_Game.js
+++ ThreeOut_Max_Game.js
@@ -3,13 +3,18 @@ const prompt = require('prompt-sync')();
 main();
 
 function main(){
-
+    console.log("Hello! Welcome to Max Out - Three Out the math game");
+    let gameMode = Number(prompt("Would you like to play the MAX OUT mode or THREE OUT mode"));
+    if(gameMode === 1)
+        maxScore();
+    else
+        threeOut();
 }
 
 function maxScore(){
-
+    console.log("maxScore");
 }
 
 function threeOut(){
-    
+    console.log("threeOut");
 }
